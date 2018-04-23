0f15a2798d8568471e21505506e6f0ac13bf1115
mcbouslog <mcbouslog@gmail.com>
2018-04-23 14:36:23 -0500

    refactor to day of month, add reset to vote page

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -7,8 +7,15 @@ const base = Rebase.createClass('https://pluto-poll.firebaseio.com');
 export default class Vote extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+    this.state = {
+      data: [],
+      intervalID: undefined,
+    };
+
     this.handleCastVote = this.handleCastVote.bind(this);
+    this.initialReset = this.initialReset.bind(this);
+    this.resetVotes = this.resetVotes.bind(this);
+    this.setTimer = this.setTimer.bind(this);
   }
 
   componentDidMount() {
@@ -16,11 +23,58 @@ export default class Vote extends React.Component {
       context: this,
       state: 'data',
       asArray: true,
+      then: this.initialReset,
     });
+    this.setTimer();
   }
 
   componentWillUnmount() {
     base.removeBinding(this.ref);
+    clearInterval(this.state.intervalID);
+  }
+
+  setTimer() {
+    const intervalID = setInterval(() => {
+      console.log('interval check');
+      const date = new Date();
+      const dayOfMonth = date.getDate();
+
+      const resetDate = new Date(this.state.data[0].reset);
+      const resetDayOfMonth = resetDate.getDate();
+      console.log('day = ', dayOfMonth);
+      console.log('resetDay = ', resetDayOfMonth);
+      if (dayOfMonth !== resetDayOfMonth) {
+        console.log('reseting...');
+        this.resetVotes(date);
+      }
+    }, 600000);
+    this.setState({ intervalID });
+  }
+
+  initialReset() {
+    console.log('initialReset check');
+    const date = new Date();
+    const dayOfMonth = date.getDate();
+
+    const resetDate = new Date(this.state.data[0].reset);
+    const resetDayOfMonth = resetDate.getDate();
+
+    console.log('day = ', dayOfMonth);
+    console.log('resetDay = ', resetDayOfMonth);
+    if (dayOfMonth !== resetDayOfMonth) {
+      console.log('reseting...');
+      this.resetVotes(date);
+    }
+  }
+
+  resetVotes(date) {
+    const newData = this.state.data;
+    for (const item of newData) {
+      item.votes = 0;
+      item.reset = date.toString();
+    }
+    console.log('reset with newData = ', newData);
+    this.setState({ data: newData });
   }
 
   handleCastVote(index) {
